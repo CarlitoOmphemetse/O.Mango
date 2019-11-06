78c551375ea8e8a9eebfd280c26f5a29cf202ca5
unknown <omphemetsecarlo@gmail.com>
2019-11-06 10:25:52 +0200

    1.1.3 sidebar

--- app.R
+++ app.R
@@ -5,14 +5,30 @@ library(ggplot2)
 library(DT)
 library(dplyr)
 
+setwd('C:/Users/MG-Lab-PC/Desktop/SPECIAL TOPICS EXAM')
+
 ui <- dashboardPage(
   dashboardHeader(),
-  dashboardSidebar(),
+  dashboardSidebar(
+    sidebarMenu(
+      uiOutput('selectWid')
+    )
+  ),
   dashboardBody()
 )
 
 server <- function(input,output){
-  output
+  
+df <- read.csv('data.csv')
+  
+  output$selectWid <- renderUI({
+    
+    selectInput(inputId = 'selectWid',label = 'Choose crime category',choices = unique(df$Category))
+    
+  })
+  
 }
 
 shinyApp(ui,server)
+
+
