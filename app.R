ee5ec4b263542369ab0947e464212c5fce146cdd
CarlitoOmphemetse <42143314+CarlitoOmphemetse@users.noreply.github.com>
2019-11-06 13:15:23 +0200

    Add files via upload
    Dockerfile and R committed

--- app.R
+++ app.R
@@ -4,15 +4,90 @@ library(shinydashboard)
 library(ggplot2)
 library(DT)
 library(dplyr)
+library(plotly)
+
+setwd('C:/Users/MG-Lab-PC/Desktop/SPECIAL TOPICS EXAM')
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = 'South African Crime Analytics'),
+  dashboardSidebar(
+    sidebarMenu(
+      
+      uiOutput('selectWid'),
+      DTOutput('dt'),
+      uiOutput('radio'),
+      menuItem('plot',tabName = 'first'),
+      menuItem('plot2',tabName = 'second')
+      
+    
+      
+    )
+  ),
+  dashboardBody(
+    fluidRow(
+      
+      tabItems(
+        tabItem(tabName = 'first'),
+        
+        tabItem(tabName = 'second',
+                
+                column(width = 10,
+                       plotOutput('bar')
+                ),
+                
+                column(width = 2)
+                
+                )
+        
+      )
+      
+      
+    )
+  )
 )
 
 server <- function(input,output){
-  output
+  
+  
+  
+  df<- read.csv('data.csv')
+
+  
+  output$selectWid <- renderUI({
+    
+    selectInput(inputId = 'selectWid',label = 'Choose crime category',choices = unique(df$Category))
+    
+  })
+  
+  output$bar <- renderPlotly({
+  
+  plotly(df$category,df$Province) 
+    
+    
+    
+  })
+  
+  output$radio <- renderUI({
+    radioButtons(inputId = 'radio',label = 'Select province',choices= unique(df$Province))
+  })
+  
+  output$dt <- renderDT({
+    
+    datatable(data,options = list(df$radio))
+    
+  })
+  
+ # render reactive()
+ 
+    
+ 
+  
+  
+  
+  
+  
 }
 
 shinyApp(ui,server)
+
+
